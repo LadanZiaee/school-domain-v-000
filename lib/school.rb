6cbe7844bf00bd854aa712014852fbeeb6133d3b
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-23 19:22:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,29 +1,31 @@
 class School
-  def initialize(school_name)
-    @school_name = school_name
-    @roster = {}
+  
+  def initialize(school)
+    @school = school #initializes name of school
+    @roster = {} #initializes a roster with empty hash
   end
   
   def roster
-    @roster
-  end
+    @roster 
+  end 
   
-def add_student(name, grade)
+  def add_student(name, grade)
     @name = name
     @grade = grade
-  if @roster.include?(grade) == false 
-    @roster[grade] = []
+    if @roster.include?(grade)==false #doesn't replace  grade when adding a school
+      @roster[grade] = [] #set key to open array to add value to an array in key
+    end
+    @roster[grade] << name
   end
-  @roster[grade] << name 
-end
-
-def grade(number)
-  @roster[number]
-end
-
-def sort
-  @roster.each do |grade, name|
-  @roster[grade] = name.sort
-end
-end
+  
+  def grade(num)
+    @roster[num]
+  end
+  
+  def sort 
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+  
 end
