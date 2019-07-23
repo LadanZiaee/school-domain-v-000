eb3e9b4788d23f54102e2dfda4115473546d2ead
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-23 19:22:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,27 @@
 class School
-  
-  def initialize(school)
-    @school = school #initializes name of school
-    @roster = {} #initializes a roster with empty hash
-  end
-  
-  def roster
-    @roster 
-  end 
-  
-  def add_student(name, grade)
+  attr_accessor :name, :roster
+
+  def initialize(name)
     @name = name
-    @grade = grade
-    if @roster.include?(grade)==false #doesn't replace  grade when adding a school
-      @roster[grade] = [] #set key to open array to add value to an array in key
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  # this method should arrange the students in each grade by alphabetical order
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
     end
-    @roster[grade] << name
+    sorted
   end
-  
-  def grade(num)
-    @roster[num]
-  end
-  
-  def sort 
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
-    end
-  end
-  
+end
 end
