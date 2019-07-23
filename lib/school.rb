50285b25e365bdfbddc1e3ac7f1ab949435a6d5e
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-23 19:24:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,19 +8,19 @@ class School
   def add_student(name, grade)
     @name = name
     @grade = grade
-    if @roster.include?(grade)==false #doesn't replace  grade when adding a school
-      @roster[grade] = [] #set key to open array to add value to an array in key
+    if roster.include?(grade)==false #doesn't replace  grade when adding a school
+      roster[grade] = [] #set key to open array to add value to an array in key
     end
-    @roster[grade] << name
+    roster[grade] << name
   end
   
   def grade(num)
-    @roster[num]
+    roster[num]
   end
   
   def sort 
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
+    roster.each do |grade, name|
+      roster[grade] = name.sort
     end
   end
 end
